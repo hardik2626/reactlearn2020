93164868f7641faede0bd72d67e87e49fd305f6a
Hardik <devanibro@gmail.com>
2020-10-28 14:51:09 +0530

    Updates

---
+++ precache-manifest.b2625c08ba21559ad99d044f21d62989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb490e99d454947a43568fa8123b844",
+    "url": "/reactlearn2020/index.html"
+  },
+  {
+    "revision": "c5b482424dcd560335ca",
+    "url": "/reactlearn2020/static/css/main.28a6bb87.chunk.css"
+  },
+  {
+    "revision": "da74203d0ab1f8077e75",
+    "url": "/reactlearn2020/static/js/2.0ecc1ba9.chunk.js"
+  },
+  {
+    "revision": "a302278f85f47d0e727312081a3a55cd",
+    "url": "/reactlearn2020/static/js/2.0ecc1ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b482424dcd560335ca",
+    "url": "/reactlearn2020/static/js/main.49443d57.chunk.js"
+  },
+  {
+    "revision": "6eed5c4b97700c692985",
+    "url": "/reactlearn2020/static/js/runtime-main.97b6abe8.js"
+  },
+  {
+    "revision": "3482f0a5d43467e3a923a0aea0b27fe0",
+    "url": "/reactlearn2020/static/media/logo.3482f0a5.png"
+  }
+]);
